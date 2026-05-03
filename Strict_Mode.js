d162b8cda029d78250475f713d30222353cb526f
Tanvi Kapurwan <125541005+TanviKapurwan23@users.noreply.github.com>
2026-05-03 08:44:59 +0530

    Update Strict_Mode.js

--- Strict_Mode.js
+++ Strict_Mode.js
@@ -1,1 +1,14 @@
 
+"use strict";
+
+let hasDriverLiscense = false;
+const passTest = true;
+
+if (passTest) {
+  hasDriverLiscense = true;
+}
+
+if (hasDriverLiscense) {
+  console.log("I can Drive");
+}
+
