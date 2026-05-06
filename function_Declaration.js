b4db4d0d891950901d6a809f84ac71c860fe36ed
Tanvi Kapurwan <125541005+TanviKapurwan23@users.noreply.github.com>
2026-05-06 18:37:12 +0530

    Update function_Declaration.js

--- function_Declaration.js
+++ function_Declaration.js
@@ -1,1 +1,22 @@
+// function declaration
 
+function calculateAge1(birthYear)
+{
+    return 2026 - birthYear;
+}
+
+const age1 = calculateAge1(2000);
+console.log(age1);
+
+
+// function expression
+
+const calculateAge2 = function (birthYear){
+    return 2026 - birthYear;
+} 
+
+//expression produces values
+
+const age2 = calculateAge2(2000);
+
+console.log(age2);
