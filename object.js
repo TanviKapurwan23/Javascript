7a1eae1a9334021eb3779771104e4ebf4169b814
Tanvi Kapurwan <125541005+TanviKapurwan23@users.noreply.github.com>
2026-05-08 19:30:51 +0530

    Create object.js

---
+++ object.js
@@ -0,0 +1,20 @@
+const TanviArray = [
+  "Tanvi",
+  "Kapurwan",
+  "2050 - 2000",
+  "Engineer",
+  ["Bella", "leena", "tira"],
+];
+
+const Tanvi = {
+  //Object
+  firstName: "Tanvi",
+  lastName: "Kapurwan",
+  age: 2050 - 2000,
+  job: "Engineer",
+  friends: ["Bella", "leena", "tira"],
+};
+
+console.log(Tanvi);
+
+
