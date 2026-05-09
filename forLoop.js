c5d5a1375012fda3f5ebced57d26a960a93f8555
Tanvi Kapurwan <125541005+TanviKapurwan23@users.noreply.github.com>
2026-05-09 16:14:57 +0530

    Create forLoop.js

---
+++ forLoop.js
@@ -0,0 +1,9 @@
+for (let rep = 1; rep <= 10; rep++) {
+  console.log(`Lifting weights repetition ${rep}`);
+}
+
+
+for (let rep = 5; rep <= 10; rep++) {
+  console.log(`Lifting weights repetition ${rep}`);
+}
+
